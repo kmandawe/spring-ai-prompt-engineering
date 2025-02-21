269156d9efc960499cde31e0c74bbaca217c897a
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-02-21 22:25:08 +0800

    Giving Clear and Precise Instructions

---
+++ src/main/java/com/kensbunker/springaipromptengineering/SpringAiPromptEngineeringApplication.java
@@ -0,0 +1,12 @@
+package com.kensbunker.springaipromptengineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiPromptEngineeringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringAiPromptEngineeringApplication.class, args);
+  }
+}
---
+++ src/test/java/com/kensbunker/springaipromptengineering/SpringAiPromptEngineeringApplicationTests.java
@@ -0,0 +1,11 @@
+package com.kensbunker.springaipromptengineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiPromptEngineeringApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
